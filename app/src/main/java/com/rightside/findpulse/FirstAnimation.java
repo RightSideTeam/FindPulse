be215c4b97e72ec6bba5e259809651769a8e80fb
Vagner Oliveira <ti.vagner@gmail.com>
2019-06-25 11:57:25 -0300

    atualiza layout

--- app/src/main/java/com/rightside/findpulse/FirstAnimation.java
+++ app/src/main/java/com/rightside/findpulse/FirstAnimation.java
@@ -18,7 +18,7 @@ public class FirstAnimation extends AwesomeSplash {
         /* you don't have to override every property */
 
         //Customize Circular Reveal
-        configSplash.setBackgroundColor(R.color.back); //any color you want form colors.xml
+        configSplash.setBackgroundColor(R.color.azulClaro); //any color you want form colors.xml
         configSplash.setAnimCircularRevealDuration(2000); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
@@ -37,14 +37,14 @@ public class FirstAnimation extends AwesomeSplash {
         configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
         configSplash.setAnimPathStrokeDrawingDuration(3000);
         configSplash.setPathSplashStrokeSize(3); //I advise value be <5
-        configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
-        configSplash.setAnimPathFillingDuration(3000);
-        configSplash.setPathSplashFillColor(R.color.Wheat); //path object filling color
+        configSplash.setPathSplashStrokeColor(R.color.Branco); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(1000);
+        configSplash.setPathSplashFillColor(R.color.azul); //path object filling color
 
 
         //Customize Title
-        configSplash.setTitleSplash("Rightside"); //change your app name here
-        configSplash.setTitleTextColor(R.color.Wheat);
+        configSplash.setTitleSplash(" Rightside "); //change your app name here
+        configSplash.setTitleTextColor(R.color.Branco);
         configSplash.setTitleTextSize(30f); //float value
         configSplash.setAnimTitleDuration(3000);
         configSplash.setAnimTitleTechnique(Techniques.FlipInX);
